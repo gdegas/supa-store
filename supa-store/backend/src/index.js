dab4339c3fb895a9a67f7cca05504f5bf89b51fc
Gerard P Degas <gdegas@edyza.com>
2019-01-25 17:39:41 -0800

    added signup mutation on backend for users to create a user

--- supa-store/backend/src/index.js
+++ supa-store/backend/src/index.js
@@ -1,21 +1,27 @@
 // let's go!
 
 // startup our node server
-require('dotenv').config({path: 'variables.env'})
+require('dotenv').config({ path: 'variables.env' });
+const cookieParser = require('cookie-parser');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
 // TODO use express middleware to handle cookies(JWT);
-
+server.express.use(cookieParser());
 // TODO use express middleware to populate current user
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  details => {
+    console.log(
+      `server is now running on port: http:/localhost:${details.port}`
+    );
   }
-}, (details) => {
-  console.log(`server is now running on port: http:/localhost:${details.port}`)
-})
+);
--- supa-store/backend/src/resolvers/Mutation.js
+++ supa-store/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   // createDog(parent, args, ctx, info) {
   //   global.dogs = global.dogs || [];
@@ -8,34 +11,61 @@ const Mutations = {
   //   return newDog
   // }
   async createItem(parent, args, ctx, info) {
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
-      }
-    }, info);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
     return item;
   },
   async updateItem(parent, args, ctx, info) {
     const updatedItem = { ...args };
     delete updatedItem.id;
-    return ctx.db.mutation.updateItem({
-      data: updatedItem,
-      where: {
-        id: args.id,
-      }
-    }, info);
+    return ctx.db.mutation.updateItem(
+      {
+        data: updatedItem,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   },
   async deleteItem(parent, args, ctx, info) {
     // find the item
-    const where = {id: args.id}
-    const item = await ctx.db.query.item({where})
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where });
     //TODO: see if the user is the owner of that item;
-    const res = await ctx.db.mutation.deleteItem(
-      {where},
-      info
-    )
+    const res = await ctx.db.mutation.deleteItem({ where }, info);
     return res;
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+    // create the jwt token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // we set a cookie on the response
+    ctx.response.cookie('token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+      httpOnly: true
+    });
+    // return the user to the browser
+    return user;
   }
 };
 
